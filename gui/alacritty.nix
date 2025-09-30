dfbb9281444fc7e90db6ba0f0f4994e1a79763aa
DuskyElf <duskyelf.dev+git@gmail.com>
2025-09-30 23:15:56 +0530

    feat: I'm done with bluish background .. make it cozy

--- gui/alacritty.nix
+++ gui/alacritty.nix
@@ -7,6 +7,7 @@
   programs.alacritty = {
     enable = true;
     settings = {
+      colors.primary.background = lib.mkForce "#181818";
       window = {
         opacity = lib.mkForce 0.85;
         padding.x = 10;
