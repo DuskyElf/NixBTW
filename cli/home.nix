95526fac745cad0e033fac1ddca9829108f68306
DuskyElf <91879372+DuskyElf@users.noreply.github.com>
2025-12-06 23:42:31 +0530

    feat: opencode

--- cli/home.nix
+++ cli/home.nix
@@ -1,9 +1,13 @@
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 {
   imports = [
     ./sh.nix
     ./git.nix
     ./tmux.nix
     ./nvim.nix
+  ];
+
+  home.packages = [
+    inputs.opencode.packages."x86_64-linux".default
   ];
 }
