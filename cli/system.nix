871dc3b76eb1ac1c84aebce6d3f5d078002f03fa
DuskyElf <git@duskyelf.me>
2026-01-15 23:09:58 +0530

    feat: gpu screen recorder

--- cli/system.nix
+++ cli/system.nix
@@ -6,6 +6,7 @@
 {
   # Very bare bones setup for root, everything else is handled by home-manager
   programs.zsh.enable = true;
+  programs.gpu-screen-recorder.enable = true;
 
   environment.systemPackages = with pkgs; [
     gcc
@@ -22,5 +23,7 @@
     dust
     ripgrep
     tealdeer
+
+    gpu-screen-recorder-gtk
   ];
 }
