d7ea5dde8c305b30aee8979e8722e63110563150
DuskyElf <duskyelf.dev+git@gmail.com>
2026-01-18 21:21:23 +0530

    refactor: opencode config

--- cli/home.nix
+++ cli/home.nix
@@ -1,13 +1,10 @@
-{ config, pkgs, inputs, ... }:
+{ ... }:
 {
   imports = [
     ./sh.nix
     ./git.nix
     ./tmux.nix
     ./nvim.nix
-  ];
-
-  home.packages = [
-    inputs.opencode.packages."x86_64-linux".default
+    ./opencode.nix
   ];
 }
