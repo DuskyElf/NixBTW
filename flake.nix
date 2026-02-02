78690fbd949ccc6f019e56e2a02a74c3fc8d6783
DuskyElf <91879372+DuskyElf@users.noreply.github.com>
2026-02-02 12:39:05 +0530

    fix: allow unfreepackage antigravity

--- flake.nix
+++ flake.nix
@@ -47,9 +47,17 @@
       ...
     }@inputs:
     let
+      lib = nixpkgs.lib;
       system = "x86_64-linux";
       pkgs-unstable = import inputs.nixpkgs-unstable {
         inherit system;
+        config = {
+          allowUnfreePredicate =
+            pkg:
+            builtins.elem (lib.getName pkg) [
+              "antigravity"
+            ];
+        };
       };
       pkgs = import nixpkgs {
         inherit system;
