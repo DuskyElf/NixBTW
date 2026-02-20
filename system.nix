1c13619a45af93081762e3659002daf0e3842cc5
DuskyElf <git@duskyelf.me>
2026-02-20 11:18:22 +0530

    feat: vim everywhere with keyd

--- system.nix
+++ system.nix
@@ -15,7 +15,6 @@
     nixpkgs.config.allowUnfree = true;
 
     console.useXkbConfig = true;
-    services.xserver.xkb.options = "ctrl:swapcaps";
 
     boot.loader = {
       systemd-boot.enable = true;
