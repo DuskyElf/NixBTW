3a79b72e7a1e233c16c2a39c3a039ceb3a18b16c
DuskyElf <91879372+DuskyElf@users.noreply.github.com>
2026-02-28 23:59:03 +0530

    feat: zram

--- hosts/asus/system.nix
+++ hosts/asus/system.nix
@@ -22,6 +22,18 @@
     ];
   };
 
+  zramSwap = {
+    enable = true;
+    algorithm = "zstd";
+    memoryPercent = 50;
+  };
+  boot.kernel.sysctl = {
+    "vm.swappiness" = 180;
+    "vm.watermark_boost_factor" = 0;
+    "vm.watermark_scale_factor" = 125;
+    "vm.page-cluster" = 0;
+  };
+
   boot.tmp.useTmpfs = true;
   fileSystems = {
     "/" = {
