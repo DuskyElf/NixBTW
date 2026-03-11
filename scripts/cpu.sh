c2b8b693ce3c21c40df6b03a04804e1a127351da
DuskyElf <91879372+DuskyElf@users.noreply.github.com>
2026-03-11 18:43:57 +0530

    feat: add fan RPM with breeze emoji to cpu.sh script

--- scripts/cpu.sh
+++ scripts/cpu.sh
@@ -5,13 +5,6 @@ temp_avg="$(sensors | grep 'Package id 0' | awk '{print $4}' | cut -b2-)"
 load_avg="$(cut -d' ' -f1 /proc/loadavg)"
 freq_avg="$(cat /proc/cpuinfo | grep 'cpu MHz' | awk '{ total += $4; count++ } END { print total/count }')"
 freq_avg="$(printf %0.f $freq_avg)"
+fan_rpm="$(sensors | grep 'cpu_fan' | awk '{print $2}')"
 
-if [ $freq_avg -gt 2000 ]; then
-  printf "󰓅"
-elif [ $freq_avg -gt 1000 ]; then
-  printf "󰾅"
-else
-  printf "󰾆"
-fi
-
-printf " %s %s@%sMHz" $temp_avg $load_avg $freq_avg
+printf "%s %s@%sMHz 💨%s" $temp_avg $load_avg $freq_avg $fan_rpm
