95f25b1168cfedb4f2104a244a262145c2702572
DuskyElf <git@duskyelf.me>
2026-04-11 21:52:45 +0530

    feat: wlsunset for my sweet eyes xD

---
+++ gui/bluelight.nix
@@ -0,0 +1,12 @@
+{ ... }:
+{
+  services.wlsunset = {
+    enable = true;
+    latitude = "28.6";
+    longitude = "77.2";
+    temperature = {
+      day = 6500;
+      night = 2700;
+    };
+  };
+}
