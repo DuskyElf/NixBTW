f2725b8efb77d4af8f2aaa2fd8ecf747642990eb
DuskyElf <duskyelf.dev+git@gmail.com>
2026-04-20 07:23:37 +0530

    feat: software level brightness for DC dimming instead of PWM dimming

--- gui/bluelight.nix
+++ gui/bluelight.nix
@@ -1,12 +1,48 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  services.wlsunset = {
-    enable = true;
-    latitude = "28.6";
-    longitude = "77.2";
-    temperature = {
-      day = 6500;
-      night = 2700;
+  systemd.user.services.night-color-temperature = {
+    Unit = {
+      Description = "Set night color temperature via wl-gammarelay";
+    };
+    Service = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.systemd}/bin/busctl --user set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q 2700";
+    };
+  };
+
+  systemd.user.timers.night-color-temperature = {
+    Unit = {
+      Description = "Timer to set night color temperature at 19:00";
+    };
+    Timer = {
+      OnCalendar = "19:00";
+      Persistent = true;
+    };
+    Install = {
+      WantedBy = [ "timers.target" ];
+    };
+  };
+
+  systemd.user.services.day-color-temperature = {
+    Unit = {
+      Description = "Set day color temperature via wl-gammarelay";
+    };
+    Service = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.systemd}/bin/busctl --user set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q 4500";
+    };
+  };
+
+  systemd.user.timers.day-color-temperature = {
+    Unit = {
+      Description = "Timer to set day color temperature at 07:00";
+    };
+    Timer = {
+      OnCalendar = "07:00";
+      Persistent = true;
+    };
+    Install = {
+      WantedBy = [ "timers.target" ];
     };
   };
 }
