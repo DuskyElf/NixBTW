dd26827702d49c84d6ca80b44a8b329354dd6fe2
DuskyElf <duskyelf.dev+git@gmail.com>
2026-04-25 13:00:51 +0530

    feat: cool cursor in kitty hehe :3

--- gui/kitty.nix
+++ gui/kitty.nix
@@ -4,5 +4,11 @@
 {
   programs.kitty = {
     enable = true;
+    settings = {
+      cursor_trail = "4";
+      cursor_trail_start_threshold = "0";
+      cursor_trail_decay = "0.1 0.4";
+      cursor_blink_interval = "0.5 ease-in-out";
+    };
   };
 }
