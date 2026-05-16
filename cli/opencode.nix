8b338bf6f5a0fe910d6a2dbe0c03fb7e74c8b063
DuskyElf <duskyelf.dev+git@gmail.com>
2026-05-16 18:33:15 +0530

    feat!: no need for opencode web server, I use piBTW

--- cli/opencode.nix
+++ cli/opencode.nix
@@ -1,6 +1,5 @@
 {
   config,
-  inputs,
   pkgs,
   pkgs-unstable,
   ...
@@ -13,21 +12,4 @@
 
   xdg.configFile."opencode".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/opencodeBTW";
-
-  systemd.user.services.opencode-web = {
-    Unit = {
-      Description = "Opencode Web Interface";
-      After = [ "network.target" ];
-    };
-    Service = {
-      ExecStart = "${
-        pkgs-unstable.opencode
-      }/bin/opencode web --port 4096";
-      Restart = "on-failure";
-      RestartSec = 5;
-    };
-    Install = {
-      WantedBy = [ "default.target" ];
-    };
-  };
 }
